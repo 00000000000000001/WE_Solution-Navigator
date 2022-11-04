78a5bed5040a52bfaa702445a2b4f0aafa75cd99
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-04 13:12:38 +0100

    Man kann der Tabelle jetzt Werte hinzufügen

--- solutions/5_1_Performanz-Messungen_von_DOM-operationen/index.js
+++ solutions/5_1_Performanz-Messungen_von_DOM-operationen/index.js
@@ -14,3 +14,23 @@ a.textContent = 'text';
 
 // outerHTML
 a.outerHTML = 'text';
+
+function appendRow(t1, t2, t3, t4) {
+    const table = document.getElementById('table');
+    var c1 = document.createElement('td');
+    c1.textContent = t1;
+    var c2 = document.createElement('td');
+    c2.textContent = t2;
+    var c3 = document.createElement('td');
+    c3.textContent = t3;
+    var c4 = document.createElement('td');
+    c4.textContent = t4;
+    let row = document.createElement('tr');
+    row.appendChild(c1);
+    row.appendChild(c2);
+    row.appendChild(c3);
+    row.appendChild(c4);
+    table.appendChild(row);
+}
+
+appendRow( "Test1", "Test2", "Test3", "test4");
