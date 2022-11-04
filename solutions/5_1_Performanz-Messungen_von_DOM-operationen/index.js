e35b7f2861381fc4e452c23b30c8c41cc6d39aa5
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-04 12:51:06 +0100

    Projekt für Aufgabe 5 angelegt

---
+++ solutions/5_1_Performanz-Messungen_von_DOM-operationen/index.js
@@ -0,0 +1,16 @@
+console.log("Bin da");
+
+const a = document.createElement('a');
+a.appendChild(document.createTextNode('text'));
+
+// innerHTML
+a.innerHTML = 'text';
+
+// innerText
+a.innerText = 'text';
+
+// textContent
+a.textContent = 'text';
+
+// outerHTML
+a.outerHTML = 'text';
