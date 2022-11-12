1bdca472f100ddfa27075e5f0569cbb3abcebf8b
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 18:01:27 +0100

    fügt String aus Textfeld zur Liste hinzu

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -3,6 +3,11 @@ function add() {
     var name = document.getElementById('name');
     var str = name.value;
     console.log(str);
+
+    var ul = document.getElementById('liste');
+    var li = document.createElement('li');
+    li.textContent = str;
+    ul.appendChild(li);
     // var li = document.createElement('li');
     // c1.textContent = t1;
 
