5ee4ca1598af7be2b2c5e05f73476e7429f0833d
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 17:55:35 +0100

    log String bei Knopfdruck

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -1,17 +1,13 @@
-function appendRow(t1, t2, t3, t4) {
-    const table = document.getElementById('table');
-    var c1 = document.createElement('td');
-    c1.textContent = t1;
-    var c2 = document.createElement('td');
-    c2.textContent = t2;
-    var c3 = document.createElement('td');
-    c3.textContent = t3;
-    var c4 = document.createElement('td');
-    c4.textContent = t4;
-    let row = document.createElement('tr');
-    row.appendChild(c1);
-    row.appendChild(c2);
-    row.appendChild(c3);
-    row.appendChild(c4);
-    table.appendChild(row);
+function add() {
+    // const list = document.getElementById('list');
+    // var name = document.getElementById('name').value;
+    console.log("name");
+    // var li = document.createElement('li');
+    // c1.textContent = t1;
+
+    // row.appendChild(c1);
+    // row.appendChild(c2);
+    // row.appendChild(c3);
+    // row.appendChild(c4);
+    // table.appendChild(row);
 }
