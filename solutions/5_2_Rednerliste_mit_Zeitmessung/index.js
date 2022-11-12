c89280ca99f71956c74bce6d96bb73d8dcc3c7b2
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 19:27:47 +0100

    Beispiel Listenelement hinzugefügt in gewünschtem Format

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -3,24 +3,31 @@ function add() {
     var str = name.value;
     var ul = document.getElementById('liste');
     var li = document.createElement('li');
+    li.id = "li" + count++;
     li.textContent = str;
     var button = document.createElement('button');
     button.textContent = "Start!";
-    button.id = count++;
+    button.id = count;
     button.onclick = function(){
         for (var i = 0; i < timers.length; ++i) {
             clearTimeout(timers[i]);
         }
         var timer = setInterval(function(){foo(button.id)}, 1000);
         timers.push(timer);
+        times.push("00:00:00");
     };
     li.appendChild(button);
     ul.appendChild(li);
 }
 
 var timers = [];
+var times = [];
 var count = 0;
 
 function foo(id) {
+    // var name = document.getElementById('li'+id);
+    // var str = name.value;
+    // str += times[id];
+
     console.log(id);
 }
