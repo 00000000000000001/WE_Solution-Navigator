262f4a03e4b8781ae15363b6534d4eef00ba0c52
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 18:49:28 +0100

    Button onclick-Funktion via Object Property hinzugefügt

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -1,22 +1,24 @@
 function add() {
     var name = document.getElementById('name');
     var str = name.value;
-    console.log(str);
-
     var ul = document.getElementById('liste');
     var li = document.createElement('li');
     li.textContent = str;
     var button = document.createElement('button');
     button.textContent = "Start!";
-    button.onclick = start;
+    button.id = count++;
+    button.onclick = function(count){setInterval(foo, 1000);};
     li.appendChild(button);
     ul.appendChild(li);
 }
 
-function start() {
-    setInterval(count, 1000);
-}
+var count = 0;
 
-function count() {
-    console.log("Test");
+// function start() {
+//     setInterval(foo, 1000);
+//     // console.log("Hallo");
+// }
+
+function foo() {
+    console.log("Hallo");
 }
