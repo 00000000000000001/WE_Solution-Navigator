5ef2f065e8b2454c9e17757182deb5ce569d238b
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 19:14:12 +0100

    Bei start eines Timers werden alle anderen Timer gestoppt

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -7,17 +7,19 @@ function add() {
     var button = document.createElement('button');
     button.textContent = "Start!";
     button.id = count++;
-    button.onclick = function(){setInterval(function(){foo(button.id)}, 1000);};
+    button.onclick = function(){
+        for (var i = 0; i < timers.length; ++i) {
+            clearTimeout(timers[i]);
+        }
+        var timer = setInterval(function(){foo(button.id)}, 1000);
+        timers.push(timer);
+    };
     li.appendChild(button);
     ul.appendChild(li);
 }
 
+var timers = [];
 var count = 0;
-
-// function start() {
-//     setInterval(foo, 1000);
-//     // console.log("Hallo");
-// }
 
 function foo(id) {
     console.log(id);
