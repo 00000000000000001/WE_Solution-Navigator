d3c5c890c1565c0d18761df622d4b3dec44eaff2
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-12 18:14:43 +0100

    Timer logt jede Sekunde einen String

--- solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
+++ solutions/5_2_Rednerliste_mit_Zeitmessung/index.js
@@ -1,5 +1,4 @@
 function add() {
-    // const list = document.getElementById('list');
     var name = document.getElementById('name');
     var str = name.value;
     console.log(str);
@@ -8,12 +7,13 @@ function add() {
     var li = document.createElement('li');
     li.textContent = str;
     ul.appendChild(li);
-    // var li = document.createElement('li');
-    // c1.textContent = t1;
+    start();
+}
 
-    // row.appendChild(c1);
-    // row.appendChild(c2);
-    // row.appendChild(c3);
-    // row.appendChild(c4);
-    // table.appendChild(row);
+function start() {
+    setInterval(count, 1000);
+}
+
+function count() {
+    console.log("Test");
 }
