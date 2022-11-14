bc0b031d1461caf46ad13ce4ce9019bd6a4e863a
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-14 18:08:37 +0100

    Kellerautomaten definiert

--- solutions/6_1_Klammerpaare/index.js
+++ solutions/6_1_Klammerpaare/index.js
@@ -1,1 +1,19 @@
 // Kellerautomat
+
+var A = {
+    Q : ["q0", "q1", "q2"],
+    Sigma : ['(', ')', '[', ']', '{', '}'],
+    Gamma : ['A', 'B', 'C', '#'],
+    delta : function() {
+        // TODO: Übergangsfunktion implementieren
+    },
+    q0 : "q0",
+    Z : '#',
+    F : "q2",
+}
+
+const stack = [];
+
+function eval() {
+    console.log("Hallo Welt!");
+}
