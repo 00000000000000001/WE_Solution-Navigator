6260b9c35cbff92f6292ad71b8b46adc1e1a3dbe
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-14 18:16:09 +0100

    Konzept für den Skriptablauf dokumentiert

--- solutions/6_1_Klammerpaare/index.js
+++ solutions/6_1_Klammerpaare/index.js
@@ -6,14 +6,22 @@ var A = {
     Gamma : ['A', 'B', 'C', '#'],
     delta : function() {
         // TODO: Übergangsfunktion implementieren
+        // TODO: delta als Generator implementieren um Zustände zu merken
     },
     q0 : "q0",
     Z : '#',
     F : "q2",
+    // Eval nimmt ein Wort "w" entgegen
+    eval : function(w) {
+        console.log(w); // Test-Ausgabe
+    }
 }
 
 const stack = [];
 
 function eval() {
-    console.log("Hallo Welt!");
+    console.log("Hallo Welt!"); // Test-Ausgabe
+    // 1. Ausdruck auslesen
+    // 2. Ausdruck als Wort w an Automaten A weitergeben
+    // 3. Eingabe je nach Rückgabewert von A.eval(w) einfärben: true=grün, false=rot
 }
