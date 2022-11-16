d893dd429c64b97c77fbcd35e31a9ae25f47bd61
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-16 19:44:01 +0100

    Klasse Vorrang iterierbar gemacht

--- solutions/6_2_Topologische_Iterierbarkeit/index.js
+++ solutions/6_2_Topologische_Iterierbarkeit/index.js
@@ -1,8 +1,22 @@
 class Vorrang {
     // Konstruktor nimmer Vorrangrelationen entgegen
-    constructor (rels) {
-        for (const r of rels){
-            console.log(r);
+    constructor (relationen) {
+        // for (const r of relationen){
+        //     console.log(r);
+        // }
+        this._relationen = relationen;
+    }
+    [Symbol.iterator]() {
+        const that = this;
+        const props = this._relationen;
+        let i = 0;
+        return {
+            next() {
+                return {
+                    value: that[props[i++]],
+                    done: i > props.length
+                }
+            }
         }
     }
 }
