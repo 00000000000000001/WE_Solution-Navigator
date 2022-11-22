5b5a0f09fcf0fc747ce5f51ae7b95727a19b0fb3
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-11-22 22:17:12 +0100

    Tests laufen durch

--- solutions/6_5_DeepCopy/index.js
+++ solutions/6_5_DeepCopy/index.js
@@ -1,52 +1,5 @@
 function deepCopy( struct ){
-    // kond. tern. Operator: condition ? exprIfTrue : exprIfFalse
-    // Array.map()
-    /*
-        const array1 = [1, 4, 9, 16];
-
-        // pass a function to map
-        const map1 = array1.map(x => x * 2);
-
-        console.log(map1);
-        // expected output: Array [2, 8, 18, 32]
-    */
-   // Objects.fromEntries(): transforms a list of key-value pairs into an object
-   /*
-        const entries = new Map([
-        ['foo', 'bar'],
-        ['baz', 42]
-        ]);
-
-        const obj = Object.fromEntries(entries);
-
-        console.log(obj);
-        // expected output: Object { foo: "bar", baz: 42 }
-   */
-  // Object.entries()
-  /*
-        const object1 = {
-        a: 'somestring',
-        b: 42
-        };
-
-        for (const [key, value] of Object.entries(object1)) {
-        console.log(`${key}: ${value}`);
-        }
-
-        // expected output:
-        // "a: somestring"
-        // "b: 42"
-  */
-
- // Algorithmus:
- // - durchlaufe jeden Eintrag in struct
-    for (const [key, value] of Object.entries(struct)) {
-        typeof(value) === "object" ? deepCopy(value) : console.log([key, value]);
-        console.log(new Map([[key, value]]));
-        console.log(Object.fromEntries(new Map([[key, value]])));
-        // console.log(typeof(value));
-        // console.log(`${key}: ${value}`);
-    }
+    return Object.fromEntries(new Map(Object.entries(struct)));
 }
 
 const struct = {
