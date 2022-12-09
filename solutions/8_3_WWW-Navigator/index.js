b9c7b50e10a887d81ed2d9619955920aab014aeb
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-12-09 18:11:13 +0100

    Knöpfe zeigen Inhalt an

--- solutions/8_3_WWW-Navigator/index.js
+++ solutions/8_3_WWW-Navigator/index.js
@@ -1,5 +1,47 @@
-console.log("test");
-
-function test( param ){
-	console.log(param);
+function myfetch(filename) {
+	return new Promise((resolve) => {
+		fetch(filename).then(content => resolve(content.json()));
+	});
 }
+
+var data;
+let map_content = new Map();
+let map_ref = new Map();
+
+async function all(param) {
+	data = await myfetch("inhalte.json");
+}
+
+function clearUntermenue(){
+	let menu = document.getElementById('untermenue');
+	menu.textContent = "";
+}
+
+function show_article( value ){
+	let article = document.getElementById('article');
+	article.textContent = "";
+	article.textContent = map_content.get(value);
+}
+
+function addKnopf( value ){
+	let knopf = document.createElement('div');
+	knopf.className = 'button';
+	knopf.addEventListener("click", function (){
+		show_article(value);
+	});
+	knopf.textContent = value;
+	let menu = document.getElementById('untermenue');
+	menu.appendChild(knopf);
+}
+
+function show( key ){
+	const menu = a[key];
+	clearUntermenue();
+	Object.entries(menu).forEach(([key, value]) => {
+		addKnopf(key);
+		map_content.set(key, value.content);
+		map_ref.set(key, value.references);
+	});
+}
+
+all();
