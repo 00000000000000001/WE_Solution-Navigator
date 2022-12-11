e793859bd44f5a0e4ddd1ac0a853c5ecb19992df
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-12-11 19:14:40 +0100

    Legende und Überschrift hinzugefügt

--- solutions/9_2_Statistik-Balkendiagramm_in_SVG/index.js
+++ solutions/9_2_Statistik-Balkendiagramm_in_SVG/index.js
@@ -21,7 +21,7 @@ function fetch_txt(filename) {
     });
 }
 
-async function f1() {
+async function f1(title) {
     const a = await fetch_txt("daten.json");
     print_entries(a);
     console.log(groups);
@@ -65,6 +65,8 @@ async function f1() {
     create_x(x_max(), height - 50 + 25);
     create_y(space_bar);
     create_bars(number);
+    create_title(title);
+    create_legende(categories(), number);
 }
 
 function x_max() {
@@ -130,15 +132,17 @@ function create_y(space) {
     }
 }
 
+const colors = ["red", "blue", "green", "yellow", "magenta", "white"];
+
 function create_bars(number) {
 
     // max = 7_599_255
     // width = 520px
     const max = x_max();
     const width = 520;
-    const div = max/(width-70);
+    const div = max / (width - 70);
 
-    const colors = ["red", "blue", "green", "yellow", "magenta", "white"];
+    
 
     const svg = document.getElementById("svg");
     let y = 60;
@@ -147,13 +151,13 @@ function create_bars(number) {
         i = 0;
         Object.entries(value).forEach(([key, value]) => {
             svg.innerHTML += `
-            <line x1="60px" y1="${y}px" x2="${value/div+60}px" y2="${y}px" stroke="${colors[i++]}" stroke-width="5" id="barDef" class="barStyle">
+            <line x1="60px" y1="${y}px" x2="${value / div + 60}px" y2="${y}px" stroke="${colors[i++]}" stroke-width="5">
                 <animate
                     attributeName="x2"
                     from="60"
-                    to="${value/div+60}"
+                    to="${value / div + 60}"
                     begin="0s"
-                    dur="1s"
+                    dur="0.7s"
                     fill="freeze"/>
             </line>
             `;
@@ -163,4 +167,38 @@ function create_bars(number) {
     }
 }
 
-f1();
+function create_title(title) {
+    const svg = document.getElementById("svg");
+    svg.innerHTML += `
+    <text x="120" y="30" fill="white" font-family="Arial, Helvetica, sans-serif" font-size="20px">${title}</text>
+    `;
+}
+function categories(){
+    let res = [];
+    // console.log(Array.from(groups)[0][1]);
+    Object.entries(Array.from(groups)[0][1]).forEach(([key, value]) => {
+        // console.log(key);
+        res.push(key);
+    });
+    return res;
+}
+function create_legende(categories, number){
+    const pixel = 8;
+    let height = number * (pixel+5)-10;
+    let x = 350;
+    let y = 100;
+    const svg = document.getElementById("svg");
+    svg.innerHTML += `
+    <rect x="${x}" y="${y}" width="70px" height="${height}px" fill="white" style="fill-opacity: .7" id="legend"/>
+    `;
+    const legend = document.getElementById("legend");
+    let i = 0;
+    for(const c of categories ){
+        y += 10;
+        svg.innerHTML += `
+        <text x="${x + 5}" y="${y}" fill="${colors[i++]}" font-family="Arial, Helvetica, sans-serif" font-size="${pixel}px">${c}</text>
+        `;
+    }
+}
+
+f1('Elektronisch übermittelte Daten');
