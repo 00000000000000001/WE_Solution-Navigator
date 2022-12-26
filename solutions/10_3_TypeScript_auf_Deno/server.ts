00b4f612247ad1bd6ac30b90f5d03fabbbe5906d
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2022-12-26 18:16:00 +0100

    Berechnung der Werte für Anzahl erfolgreich

--- solutions/10_3_TypeScript_auf_Deno/server.ts
+++ solutions/10_3_TypeScript_auf_Deno/server.ts
@@ -4,29 +4,29 @@ const res = await fetch(url);
 const body = new Uint8Array(await res.arrayBuffer());
 const str = new TextDecoder().decode(body);
 
-let min_anz:number;
-let min_diff:number;
-let min_fall:number;
-let min_inz:number;
-let min_tod:number;
+let min_anz:number = Number.MAX_SAFE_INTEGER;
+let min_diff:number = Number.MAX_SAFE_INTEGER;
+let min_fall:number = Number.MAX_SAFE_INTEGER;
+let min_inz:number = Number.MAX_SAFE_INTEGER;
+let min_tod:number = Number.MAX_SAFE_INTEGER;
 
-let max_anz:number;
-let max_diff:number;
-let max_fall:number;
-let max_inz:number;
-let max_tod:number;
+let max_anz:number = 0;
+let max_diff:number = 0;
+let max_fall:number = 0;
+let max_inz:number = 0;
+let max_tod:number = 0;
 
-let avg_anz:number;
-let avg_diff:number;
-let avg_fall:number;
-let avg_inz:number;
-let avg_tod:number;
+let avg_anz:number = 0;
+let avg_diff:number = 0;
+let avg_fall:number = 0;
+let avg_inz:number = 0;
+let avg_tod:number = 0;
 
-let sum_anz:number;
-let sum_diff:number;
-let sum_fall:number;
-let sum_inz:number;
-let sum_tod:number;
+let sum_anz:number = 0;
+let sum_diff:number = 0;
+let sum_fall:number = 0;
+let sum_inz:number = 0;
+let sum_tod:number = 0;
 
 import {
     DOMParser,
@@ -40,10 +40,23 @@ import {
     let doc = new DOMParser().parseFromString(str,"text/html");
     const tds = doc.querySelectorAll("td");
 
-    let i:number = 0;
-    for (const td of tds) {
+    for (var i = 0; i < 16*6; i++) {
+        console.log(tds[i].textContent);
+        let s = tds[i].textContent;
+        s = s.replaceAll('.', '');
+        s = s.replaceAll(',', '.');
         switch (i%6){
             case 1: // anz
+                // min
+                if (Number(s) < min_anz){
+                    min_anz = Number(s);
+                }
+                // max
+                if (Number(s) > max_anz){
+                    max_anz = Number(s);
+                }
+                //sum
+                sum_anz += Number(s);
                 break;
             case 2: // diff
                 break;
@@ -56,6 +69,11 @@ import {
             default:
                 
         }
-        console.log(td.textContent);
     }
+    console.log(`
+    min. Anzahl: ${min_anz}
+    max. Anzahl: ${max_anz}
+    avg. Anzahl: ${sum_anz / 16}
+    sum. Anzahl: ${sum_anz}
+    `);
   })();
