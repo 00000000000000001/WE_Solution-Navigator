9bc34d4a368999f67ef6bebc7659780a1629b163
00000000000000001 <77029328+00000000000000001@users.noreply.github.com>
2023-01-25 21:19:57 +0100

    Lösung Aufgabe 11.1

---
+++ solutions/11_1_Komponente_in_Vue.js/Vue.js
@@ -0,0 +1,11 @@
+/*
+ Schreiben Sie eine Vue.js Single File Component mit einem Text-Eingabefeld und 3 Ausgabefeldern, 
+ in denen man während des Tippens sehen kann, (a) wie viele Buchstaben (b) wie viele Leerzeichen 
+ und (c) wie viele Worte man in das Text-Eingabefeld bereits eingegeben hat.
+
+Betten Sie Ihre Komponente in eine Webseite zweimal ein und testen Sie, ob beide Komponenten 
+unabhängig voneinander sind. 
+*/
+
+let eingabe = document.createElement('input');
+eingabe.type = 'textfield';
